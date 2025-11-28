675d29c7e9c73aa81639dd95c4d11f3e259441e8
Juan Suache <juansuache2023@gmail.com>
2025-11-28 18:57:18 -0500

    Connecting DB - HTTPS : GET

--- src/app.js
+++ src/app.js
@@ -1,6 +1,8 @@
 import express from "express";
+import cors from "cors"
 import categoriaRoutes from "./routers/categorias.routes.js"
 const app = express();
 app.set("port", 5000)
+app.use(cors());
 app.use("/api/categorias", categoriaRoutes)
 export default app;
